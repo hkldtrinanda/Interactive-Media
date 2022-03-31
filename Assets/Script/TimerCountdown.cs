e348530f4f780b20e8f59fe8f1afbb652bd104d4
hkldtrinanda <hkldtrinanda12@gmail.com>
2022-04-01 01:45:33 +0700

    aaa

---
+++ Assets/Script/TimerCountdown.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerCountdown : MonoBehaviour
+{
+    public string scene;
+
+    public float timeLeft = 3.0f;
+    public Text startText; // used for showing countdown from 3, 2, 1 
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        startText.text = (timeLeft).ToString("0");
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
